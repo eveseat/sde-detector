4e60d364879bbc82b75e78d037bf74d780c91a00
Leon Jacobs <leonja511@gmail.com>
2016-04-30 13:23:31 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import requests
+import os
+import sys
+from slacker import Slacker
+
+HASH_URL = 'https://www.fuzzwork.co.uk/dump/mysql-latest.tar.bz2.md5'
+HASH_FILE = 'hash_file'
+SLACK_API_KEY = ''
+SLACK_CHANNEL = '#development'
+
+# Ask Fuzzworks whats the current hash
+r = requests.get(HASH_URL)
+current_hash = r.text.split(' ')[0]
+
+hash_file = os.path.join(
+    os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))), HASH_FILE)
+
+# Check what is the last hash we knew of
+with open(hash_file, 'r') as f:
+    known_hash = f.read().strip()
+
+# If hashes are still the same, do nothing
+if known_hash == current_hash:
+    sys.exit(1)
+
+with open(hash_file, 'wb') as f:
+    f.write(current_hash)
+
+slack = Slacker(SLACK_API_KEY)
+slack.chat.post_message(SLACK_CHANNEL,
+                        'Possible new SDE available at https://www.fuzzwork.co.uk/dump/ !\n' +
+                        'Old hash: {hash}\n'.format(hash=known_hash) +
+                        'New hash: {hash}'.format(hash=current_hash),
+                        username='SDE Bot')
